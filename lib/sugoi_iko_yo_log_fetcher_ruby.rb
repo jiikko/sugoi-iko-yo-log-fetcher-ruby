dc104e9723a080c6c1e75c334a3c7a71e5ec36ab
jiikko <n905i.1214@gmail.com>
2016-12-04 20:39:55 +0900

    fix bug

--- lib/sugoi_iko_yo_log_fetcher_ruby.rb
+++ lib/sugoi_iko_yo_log_fetcher_ruby.rb
@@ -9,22 +9,24 @@ module SugoiIkoYoLogFetcherRuby
       @end_on = args[1]
     end
 
-    def run
+    def run!
       if @end_on.nil?
         fetch_log(@start_on)
-      end
-
-      Date.parse(@start_on)..Date.parse(@end_on).each do |date|
-        fetch_log(date)
+      else
+        Date.parse(@start_on)..Date.parse(@end_on).each do |date|
+          fetch_log(date)
+        end
       end
     end
 
     def valid?
       case
-      when @start_on.nil? && @end_on.nil?
-        @error_message << 'not found args.'
-      when @start_on > @end_on
-        @error_message << 'invalid dates. endのほうが大きく'
+      when @start_on.nil?
+        @error_messages << 'not found args.'
+        false
+      when @end_on && (@start_on > @end_on)
+        @error_messages << 'invalid dates. endのほうを大きく'
+        false
       else
         true
       end
@@ -43,7 +45,7 @@ module SugoiIkoYoLogFetcherRuby
     end
   end
 
-  def build_cli(argv)
+  def self.build_cli(argv)
     CLI.new(argv)
   end
 end
