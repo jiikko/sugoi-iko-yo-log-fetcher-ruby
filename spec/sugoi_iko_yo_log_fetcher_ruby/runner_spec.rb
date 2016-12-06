83439b01479ccfaa5759c673b077e3103bf28ff2
jiikko <n905i.1214@gmail.com>
2016-12-06 22:38:23 +0900

    fix spec

--- spec/sugoi_iko_yo_log_fetcher_ruby/runner_spec.rb
+++ spec/sugoi_iko_yo_log_fetcher_ruby/runner_spec.rb
@@ -5,24 +5,24 @@ describe SugoiIkoYoLogFetcherRuby::Runner do
     require 'tmpdir'
 
     before(:each) do
+      mock_s3_object_class = Struct.new(:key)
+      mock_s3_object = mock_s3_object_class.new('logs/test/2015/12/12_0.gz')
+      allow_any_instance_of(SugoiIkoYoLogFetcherRuby::Runner).to receive(:bucket_objects).and_return([mock_s3_object])
+      allow(mock_s3_object).to receive(:get).and_return(nil)
       allow_any_instance_of(SugoiIkoYoLogFetcherRuby::Runner).to receive(:setup_aws_sdk!).and_return(nil)
     end
 
     it 'be success' do
       correct_pwd = Dir.pwd
-      mock_s3_object_class = Struct.new(:key)
-      mock_s3_object = mock_s3_object_class.new('logs/test/2015/12/12_0.gz')
-      allow(mock_s3_object).to receive(:get).and_return(nil)
       begin
         dir_name = Dir.mktmpdir
         Dir.chdir(dir_name)
         runner = SugoiIkoYoLogFetcherRuby::Runner.new(Date.new(2011, 1, 1))
-        allow_any_instance_of(SugoiIkoYoLogFetcherRuby::Runner).to receive(:bucket_objects).and_return([mock_s3_object])
         runner.download!
         expect(File.ftype("#{dir_name}/logs/test/2015/12/")).to eq('directory')
-        expect(File.exists?("#{dir_name}/logs/test/2015/12/12_0.gz")).to eq(true)
-        Dir.chdir(correct_pwd)
+        expect(File.ftype("#{dir_name}/logs/test/2015/12/12_0.gz")).to eq('file')
       ensure
+        Dir.chdir(correct_pwd)
         FileUtils.remove_entry_secure(dir_name) if File.exists?(dir_name)
       end
     end
